90dcd3bac1199ea06b0310cd0752d1300a64def7
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-08-03 17:29:03 +0100

    completed specs

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -18,6 +18,15 @@ describe('Thermostat', function(){
       thermostat.up();
       expect(thermostat.temperature).toEqual(DEFAULT_TEMP + 1);
     })
+
+    it('cannot go above maximum temperature', function(){
+      for(var i = 0; i < (thermostat.maximumTemp - DEFAULT_TEMP); i++){
+        thermostat.up();
+      }
+      expect(function(){
+        thermostat.up();
+      }).toThrowError("Cannot go above maximum temperature");
+    })
   })
 
   describe('down button', function(){
@@ -27,13 +36,53 @@ describe('Thermostat', function(){
     })
 
     it('cannot go below minimum temperature',function(){
-      for(var i = 0; i < 10; i++){
-        console.log(i);
+      for(var i = 0; i < MINIMUM_TEMP; i++){
         thermostat.down();
       }
       expect(function(){
         thermostat.down();
       }).toThrowError("Cannot go below minimum temperature");
+    })
+  })
+
+  describe('powerSaving', function(){
+    it('changes maximum temperature when power saving', function(){
+      expect(thermostat.maximumTemp).toEqual(MAXIMUM_TEMP['powerSaving']);
+    })
+
+    it('changes maximum temperature when not power saving', function(){
+      thermostat.togglePowerSaving();
+      expect(thermostat.maximumTemp).toEqual(MAXIMUM_TEMP['noPowerSaving']);
+    })
+  })
+
+  describe('reset temperature', function(){
+    it('sets the temperature to default temperature', function(){
+      for(var i = 0; i < MINIMUM_TEMP; i++){
+        thermostat.down();
+      }
+      thermostat.resetTemp();
+      expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
+    })
+  })
+
+  describe('energy usage', function(){
+    it('shows low for low usage', function(){
+      for(var i = 0; i < MINIMUM_TEMP; i++){
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('Low');
+    })
+
+    it('shows medium for medium usage', function(){
+      expect(thermostat.energyUsage()).toEqual('Medium');
+    })
+
+    it('shows high for high usage', function(){
+      for(var i = 0; i < (thermostat.maximumTemp - DEFAULT_TEMP); i++){
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('High');
     })
   })
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,22 +1,60 @@
 'use strict';
 
 const DEFAULT_TEMP = 20;
+const MINIMUM_TEMP = 10;
+const MAXIMUM_TEMP = {powerSaving: 25, noPowerSaving: 32}
+const ENERGY = {low: 18, medium: 25}
 
 var Thermostat = function(temperature = DEFAULT_TEMP){
   this.temperature = temperature;
+  this.powerSaving = true;
+  this.maximumTemp = MAXIMUM_TEMP['powerSaving'];
+  this.minimumTemp = MINIMUM_TEMP;
 }
 
 Thermostat.prototype.up = function(){
-  this.temperature += 1;
+  if (this.checkMaximumTemp()){
+    throw new Error("Cannot go above maximum temperature");
+  }
+  this.temperature++;
 }
 
 Thermostat.prototype.down = function(){
-  if (this.minimum_temp()){
+  if (this.checkMinimumTemp()){
     throw new Error("Cannot go below minimum temperature");
   }
-  this.temperature -= 1;
+  this.temperature--;
 }
 
-Thermostat.prototype.minimum_temp = function(){
-  return this.temperature <= 10;
+Thermostat.prototype.checkMinimumTemp = function(){
+  return this.temperature <= MINIMUM_TEMP;
+}
+
+Thermostat.prototype.checkMaximumTemp = function(){
+  return this.temperature >= this.maximumTemp;
+}
+
+Thermostat.prototype.togglePowerSaving = function(){
+  if (this.powerSaving){
+    this.powerSaving = false;
+    this.maximumTemp = MAXIMUM_TEMP['noPowerSaving'];
+  }
+  else {
+    this.powerSaving = true;
+    this.maximumTemp = MAXIMUM_TEMP['powerSaving'];
+  }
+}
+
+Thermostat.prototype.resetTemp = function(){
+  this.temperature = DEFAULT_TEMP;
+}
+
+Thermostat.prototype.energyUsage = function(){
+  if (this.temperature < ENERGY['low']){
+    return 'Low';
+  }
+  if (this.temperature < ENERGY['medium']){
+    return 'Medium';
+  }
+  return 'High';
 }
