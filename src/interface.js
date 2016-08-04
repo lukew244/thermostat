80027305fd3705ca47c98de8a8b8f26bba5565ea
lukew244 <luke.webster86@gmail.com>
2016-08-04 14:52:20 +0100

    Adds temperature to page

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,4 @@
-// $(document).ready(function() {
-//   var thermostat = new Thermostat();
-//   $('#temperature').text(thermostat.temperature);
-// });
-
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
-})
+});
